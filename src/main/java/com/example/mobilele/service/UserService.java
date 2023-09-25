3109343c5f5ab7221dbab405a486412a6a236b79
GalyaSemova <semovagalina@gmail.com>
2023-09-25 12:28:56 +0300

    UserRegistrationController created

---
+++ src/main/java/com/example/mobilele/service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.mobilele.service;
+
+import com.example.mobilele.model.dto.UserRegistrationDTO;
+
+public interface UserService {
+
+    void registerUser(UserRegistrationDTO userRegistrationDTO);
+}
---
+++ src/main/java/com/example/mobilele/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.mobilele.service.impl;
+
+import com.example.mobilele.model.dto.UserRegistrationDTO;
+import com.example.mobilele.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public void registerUser(UserRegistrationDTO userRegistrationDTO) {
+        System.out.println(userRegistrationDTO);
+
+    }
+}
