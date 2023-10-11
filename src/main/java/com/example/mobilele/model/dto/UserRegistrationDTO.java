c5e36919fd6514bc3d2036d6ae88f65ba781b7d4
GalyaSemova <semovagalina@gmail.com>
2023-10-11 14:35:48 +0300

    Unique email validator and field match type validator created

--- src/main/java/com/example/mobilele/model/dto/UserRegistrationDTO.java
+++ src/main/java/com/example/mobilele/model/dto/UserRegistrationDTO.java
@@ -1,8 +1,18 @@
 package com.example.mobilele.model.dto;
 
-public record UserRegistrationDTO(String firstName,
-                                  String lastName,
-                                  String email,
+import com.example.mobilele.model.validation.FieldMatch;
+import com.example.mobilele.model.validation.UniqueUserEmail;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+@FieldMatch(
+    first = "password",
+    second = "confirmPassword",
+    message = "Passwords should match"
+)
+public record UserRegistrationDTO(@NotEmpty String firstName,
+                                  @NotEmpty String lastName,
+                                  @NotNull @Email @UniqueUserEmail String email,
                                   String password,
                                   String confirmPassword) {
 }
