39951c481f363a83c274a3ed9213c1159a2e2600
GalyaSemova <semovagalina@gmail.com>
2023-10-11 08:06:46 +0300

    NamedEntityGraph DEmo in BrandEntity

--- src/main/java/com/example/mobilele/model/entity/BrandEntity.java
+++ src/main/java/com/example/mobilele/model/entity/BrandEntity.java
@@ -1,16 +1,35 @@
 package com.example.mobilele.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.List;
 
 
 @Entity
 @Table(name = "brands")
+@NamedEntityGraph(
+        name = "brandWithModels",
+        attributeNodes = @NamedAttributeNode("models")
+)
 public class BrandEntity extends BaseEntity{
     @Column(unique = true, nullable = false)
     private String brand;
-//TODO: rename to 'name' and make it unique
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            mappedBy = "brand")
+//    @Fetch(value = FetchMode.SUBSELECT)
+    private List<ModelEntity> models;
+
+    public List<ModelEntity> getModels() {
+        return models;
+    }
+
+    public void setModels(List<ModelEntity> models) {
+        this.models = models;
+    }
+
     public String getBrand() {
         return brand;
     }
--- src/main/java/com/example/mobilele/repository/BrandRepository.java
+++ src/main/java/com/example/mobilele/repository/BrandRepository.java
@@ -1,9 +1,22 @@
 package com.example.mobilele.repository;
 
 import com.example.mobilele.model.entity.BrandEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BrandRepository extends JpaRepository<BrandEntity, Long> {
+//    @Query("SELECT b FROM BrandEntity b " +
+//            "JOIN FETCH b.models ")
+//    public List<BrandEntity> getAllBrands();
+    @EntityGraph(
+            value = "brandWithModels",
+            attributePaths = "models"
+    )
+    @Query("SELECT b FROM BrandEntity b")
+    public List<BrandEntity> getAllBrands();
 }
